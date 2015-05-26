b91628ab9b51044be4af175bac9f05294d071f34
Carlos Abalde <carlos.abalde@gmail.com>
2015-05-26 15:12:26 +0200

    Improved test runner (preparing for Redis Cluster tests)

--- src/tests/runner.sh
+++ src/tests/runner.sh
@@ -5,39 +5,45 @@
 ##
 set -e
 set -x
-REDIS1_PORT=50001
-REDIS2_PORT=50002
+REDIS_SERVERS=2
+REDIS_START_PORT=40000
+REDIS_CLUSTER_SERVERS=9
+REDIS_CLUSTER_START_PORT=50000
+REDIS_CLUSTER_REPLICAS=2
+VTC_DEFINES=""
 TMP=`mktemp -d`
 
 ##
 ## Cleanup callback.
 ##
 cleanup() {
-    kill -9 $(cat "$1/redis1.pid")
-    kill -9 $(cat "$1/redis2.pid")
+    for INDEX in `seq 1 $REDIS_SERVERS`; do
+        kill -9 $(cat "$1/redis-master$INDEX.pid")
+    done
     rm -rf "$1"
 }
 
 ##
-## Temporarily launch a couple of Redis servers.
+## Launch standalone Redis servers.
 ##
-cat > "$TMP/redis1.conf" <<EOF
+for INDEX in `seq 1 $REDIS_SERVERS`; do
+    cat > "$TMP/redis-master$INDEX.conf" <<EOF
 daemonize yes
-port $REDIS1_PORT
+port $((REDIS_START_PORT+INDEX))
 bind 127.0.0.1
-unixsocket $TMP/redis1.sock
-pidfile $TMP/redis1.pid
+unixsocket $TMP/redis-master$INDEX.sock
+pidfile $TMP/redis-master$INDEX.pid
 EOF
-redis-server "$TMP/redis1.conf"
+    redis-server "$TMP/redis-master$INDEX.conf"
+    VTC_DEFINES="\
+        $VTC_DEFINES \
+        -Dredis_master${INDEX}_address=127.0.0.1:$((REDIS_START_PORT+INDEX)) \
+        -Dredis_master${INDEX}_socket=$TMP/redis-master$INDEX.sock"
+done
 
-cat > "$TMP/redis2.conf" <<EOF
-daemonize yes
-port $REDIS2_PORT
-bind 127.0.0.1
-unixsocket $TMP/redis2.sock
-pidfile $TMP/redis2.pid
-EOF
-redis-server "$TMP/redis2.conf"
+##
+## Launch clustered Redis servers.
+##
 
 ##
 ## Register cleanup callback.
@@ -47,7 +53,10 @@ trap "cleanup $TMP" EXIT
 ##
 ## Execute wrapped command.
 ##
-$@ -Dredis1_address="127.0.0.1:$REDIS1_PORT" \
-   -Dredis2_socket="$TMP/redis1.sock" \
-   -Dredis2_address="127.0.0.1:$REDIS2_PORT" \
-   -Dredis2_socket="$TMP/redis2.sock"
+$@ \
+    -Dredis_servers=$REDIS_SERVERS \
+    -Dredis_start_port=$REDIS_START_PORT \
+    -Dredis_cluster_servers=$REDIS_CLUSTER_SERVERS \
+    -Dredis_cluster_start_port=$REDIS_CLUSTER_START_PORT \
+    -Dredis_cluster_replicas=$REDIS_CLUSTER_REPLICAS \
+    $VTC_DEFINES
