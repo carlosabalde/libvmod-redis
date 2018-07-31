75773d57384e2417ffbd2e0e4deef44124326f8b
Carlos Abalde <carlos.abalde@gmail.com>
2018-07-31 16:46:13 +0200

    Fix wrong struct field name

--- src/vmod_redis.c
+++ src/vmod_redis.c
@@ -598,7 +598,7 @@ vmod_db_push(
                 flush_task_state(state);
             }
         }
-        state->execution.argv[state->execution.argc++] = arg;
+        state->command.argv[state->command.argc++] = arg;
     } else {
         REDIS_LOG_ERROR(ctx,
             "Failed to push argument (db=%s, limit=%d)",
