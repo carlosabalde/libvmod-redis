d181db974d37b7fea3b49c571571033aaadd1e0a
Carlos Abalde <carlos.abalde@gmail.com>
2023-04-28 19:35:32 +0200

    Fix potential segmentation fault due to wrong logging in Sentinel thread

--- src/sentinel.c
+++ src/sentinel.c
@@ -351,8 +351,8 @@ sentinel_loop(void *object)
                                     "AUTH %s", state->password) != REDIS_OK) {
                                 REDIS_LOG_ERROR(NULL,
                                     "Failed to enqueue asynchronous Sentinel AUTH command (error=%d, sentinel=%s:%d): %s",
-                                    isentinel->host, isentinel->port,
-                                    HIREDIS_ERRSTR(isentinel->context));
+                                    isentinel->context.err, isentinel->host,
+                                    isentinel->port, HIREDIS_ERRSTR(isentinel->context));
                                 redisAsyncFree(isentinel->context);
                                 isentinel->context = NULL;
                             }
@@ -365,8 +365,8 @@ sentinel_loop(void *object)
                                     "HELLO %d", state->protocol) != REDIS_OK) {
                                 REDIS_LOG_ERROR(NULL,
                                     "Failed to enqueue asynchronous Sentinel HELLO command (error=%d, sentinel=%s:%d): %s",
-                                    isentinel->host, isentinel->port,
-                                    HIREDIS_ERRSTR(isentinel->context));
+                                    isentinel->context.err, isentinel->host,
+                                    isentinel->port, HIREDIS_ERRSTR(isentinel->context));
                                 redisAsyncFree(isentinel->context);
                                 isentinel->context = NULL;
                             }
@@ -378,8 +378,8 @@ sentinel_loop(void *object)
                                 SUBSCRIPTION_COMMAND) != REDIS_OK) {
                             REDIS_LOG_ERROR(NULL,
                                 "Failed to enqueue asynchronous Sentinel subscription command (error=%d, sentinel=%s:%d): %s",
-                                isentinel->host, isentinel->port,
-                                HIREDIS_ERRSTR(isentinel->context));
+                                isentinel->context.err, isentinel->host,
+                                isentinel->port, HIREDIS_ERRSTR(isentinel->context));
                             redisAsyncFree(isentinel->context);
                             isentinel->context = NULL;
                         }
